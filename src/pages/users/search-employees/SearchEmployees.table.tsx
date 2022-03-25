7b3f5ef8549c22d72c744de6f4139f176c2a81a7
tomimarkus991 <tomimarkusalber@gmail.com>
2022-03-25 09:47:36 +0200

    Make buttons optional, add types

--- src/pages/users/search-employees/SearchEmployees.table.tsx
+++ src/pages/users/search-employees/SearchEmployees.table.tsx
@@ -1,10 +1,12 @@
+import { Column } from "react-table";
+
 import { TwoMouseEventActionButtons, IDefaultActionButtons } from "components/widgets";
 
 export const employeesTableColumns = ({
   onDetailsButtonClick,
   onRemoveButtonClick,
 }: IDefaultActionButtons) => {
-  return [
+  const tableArray = [
     {
       accessor: "id",
       Header: "id",
@@ -41,6 +43,11 @@ export const employeesTableColumns = ({
       accessor: "startDate",
       Header: "Hire Date",
     },
-    TwoMouseEventActionButtons({ onDetailsButtonClick, onRemoveButtonClick }),
-  ];
+  ] as Array<Column>;
+
+  if (onDetailsButtonClick && onRemoveButtonClick) {
+    tableArray.push(TwoMouseEventActionButtons({ onDetailsButtonClick, onRemoveButtonClick }));
+  }
+
+  return tableArray;
 };
